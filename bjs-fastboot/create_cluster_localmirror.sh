0e0019ca8acb115336a35c1228f6e38de76352ce
Pahud Hsieh <pahudnet@gmail.com>
2018-07-24 14:59:48 +0800

    - support Kops 1.9.2 with Kubernetes 1.9.8 - support latest CoreOS AMI - 1745.7.0

--- bjs-fastboot/create_cluster_localmirror.sh
+++ bjs-fastboot/create_cluster_localmirror.sh
@@ -6,18 +6,19 @@ cluster_name='cluster.bjs.k8s.local'
 
 # official CoreOS AMI
 #ami='ami-39ee3154'
-ami='ami-555a8438'
+#ami='ami-555a8438'
+ami='ami-2a875e47'
 
 
 # change this to your vpcid
 vpcid='vpc-c1e040a5'
 
-KUBERNETES_VERSION='v1.9.6'
-KOPS_VERSION='1.9.1'
+KUBERNETES_VERSION='v1.9.8'
+KOPS_VERSION='1.9.2'
 kubernetesVersion="https://s3.cn-north-1.amazonaws.com.cn/kubernetes-release/release/$KUBERNETES_VERSION"
 
-export CNI_VERSION_URL="https://s3.cn-north-1.amazonaws.com.cn/kubernetes-release/network-plugins/cni-plugins-amd64-v0.6.0.tgz"
-export CNI_ASSET_HASH_STRING="d595d3ded6499a64e8dac02466e2f5f2ce257c9f"
+#export CNI_VERSION_URL="https://s3.cn-north-1.amazonaws.com.cn/kubernetes-release/network-plugins/cni-plugins-amd64-v0.6.0.tgz"
+#export CNI_ASSET_HASH_STRING="d595d3ded6499a64e8dac02466e2f5f2ce257c9f"
 export KOPS_BASE_URL=https://s3.cn-north-1.amazonaws.com.cn/kubeupv2/kops/${KOPS_VERSION}/
 
 kops create cluster \
--- zhy-fastboot/create_cluster_localmirror.sh
+++ zhy-fastboot/create_cluster_localmirror.sh
@@ -6,17 +6,18 @@ cluster_name='cluster.zhy.k8s.local'
 
 # official CoreOS AMI
 #ami='ami-e7958185'
-ami='ami-06a0b464'
+#ami='ami-06a0b464'
+ami='ami-c9abbfab'
 
 # change this to your vpcid
 vpcid='vpc-bb3e99d2'
 
-KUBERNETES_VERSION='v1.9.6'
-KOPS_VERSION='1.9.1'
+KUBERNETES_VERSION='v1.9.8'
+KOPS_VERSION='1.9.2'
 kubernetesVersion="https://s3.cn-north-1.amazonaws.com.cn/kubernetes-release/release/$KUBERNETES_VERSION"
 
-export CNI_VERSION_URL="https://s3.cn-north-1.amazonaws.com.cn/kubernetes-release/network-plugins/cni-plugins-amd64-v0.6.0.tgz"
-export CNI_ASSET_HASH_STRING="d595d3ded6499a64e8dac02466e2f5f2ce257c9f"
+#export CNI_VERSION_URL="https://s3.cn-north-1.amazonaws.com.cn/kubernetes-release/network-plugins/cni-plugins-amd64-v0.6.0.tgz"
+#export CNI_ASSET_HASH_STRING="d595d3ded6499a64e8dac02466e2f5f2ce257c9f"
 export KOPS_BASE_URL=https://s3.cn-north-1.amazonaws.com.cn/kubeupv2/kops/${KOPS_VERSION}/
 export NODEUP_URL=${KOPS_BASE_URL}linux/amd64/nodeup
 export PROTOKUBE_IMAGE=${KOPS_BASE_URL}images/protokube.tar.gz
@@ -33,3 +34,4 @@ kops create cluster \
      --kubernetes-version="$kubernetesVersion" \
      --ssh-public-key="~/.ssh/id_rsa.pub"
      
+     #--networking="amazon-vpc-routed-eni" \
